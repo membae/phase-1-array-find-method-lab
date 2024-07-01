63e911f34dcc2a0b1cb5fcb5d19d2c2dc62f0c02
membae <membaphilip1@gmail.com>
2024-07-01 14:14:23 +0300

    added

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function superbowlWin(records){
+    const winrecord=records.find(record=>record.result==="W");
+    return winrecord?winrecord.year:undefined;
+}
+    const array=[
+        {
+            year:"2015",result:"W"
+        },
+        {
+            year:"2014",result:"N/A"
+        },
+        {
+            year:"2012",result:"L"
+        }
+    ];
+    console.log(superbowlWin(array));
